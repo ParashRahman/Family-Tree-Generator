0a178cc9f8b1d6417a9b5c3a0e2389ea323b43df
ParashRahman <parash@ualberta.ca>
2017-06-23 10:26:10 -0600

    considering memory options

--- prototype/family_tree.cpp
+++ prototype/family_tree.cpp
@@ -1,18 +1,27 @@
+#include <vector>
+
 #include "family_tree.h"
 #include "person.h"
 
 typedef std::shared_ptr<Person> person_ptr;
+typedef std::weak_ptr<Person> weak_person_ptr;
 typedef unsigned long long ID_type;
 typedef unsigned long long gen_type;
 typedef std::vector<ID_type> friend_vector;
 typedef std::unordered_map< ID_type, std::vector<ID_type> > children_map;
 
 FamilyTree::FamilyTree() {
-  
+  o_god = person_ptr(new Person("G.O.D.", 0, 0, 0));
+  people_cache[o_god->get_ID()] = o_god;
+  needs_updating[o_god->get_ID()] = false;
+  gen_mapping[0].push_back(o_god->get_ID());
+
+  // make adam and eve
+  make_kids(o_god->get_ID(), o_god->get_ID(), 2);
 }
 
 void FamilyTree::visit_person(ID_type id) {
-
+  
 }
 
 children_map FamilyTree::generate_love_lives(ID_type id) {
@@ -20,5 +29,13 @@ children_map FamilyTree::generate_love_lives(ID_type id) {
 }
 
 friend_vector FamilyTree::generate_friendships(ID_type id) {
+
+}
+
+std::vector<ID_type> FamilyTree::make_kids(ID_type person1, ID_type person2, int number) {
+
+}
+
+void make_friends(ID_type person1, ID_type person2) {
 
 }
--- prototype/family_tree.h
+++ prototype/family_tree.h
@@ -8,6 +8,7 @@
 #include "person.h"
 
 typedef std::shared_ptr<Person> person_ptr;
+typedef std::weak_ptr<Person> weak_person_ptr;
 typedef unsigned long long ID_type;
 typedef unsigned long long gen_type;
 typedef std::vector<ID_type> friend_vector;
@@ -20,15 +21,18 @@ class FamilyTree {
 
   // these maps allow caching of people and keeping track
   //   of which people need to be reprocedurally generated
-  // TODO: weak pointers or they'll last forever
-  std::unordered_map<ID_type, person_ptr> people_cache;
+  std::unordered_map<ID_type, weak_person_ptr> people_cache;
   std::unordered_map<ID_type, bool> needs_updating;
-  std::unordered_map< gen_type, std::vector<ID_type> > gen_mapping; 
+  std::unordered_map< gen_type, std::vector<ID_type> > gen_mapping;
+
+  person_ptr o_god;
  public:
   FamilyTree();
   void visit_person(ID_type id);
   children_map generate_love_lives(ID_type id);
   friend_vector generate_friendships(ID_type id);
+  std::vector<ID_type> make_kids(ID_type person1, ID_type person2, int number);
+  void make_friends(ID_type person1, ID_type person2);
 };
 
 #endif
